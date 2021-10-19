70b696078bb36ec8488835b5c31950633f0c4cbc
marwan <marwangalal746>
2021-10-20 01:10:02 +0200

    endpoints to convert from usd to eur and vice-versa

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 )
 var key = []byte("currencyTask")
@@ -103,13 +104,71 @@ func GetCurrency(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonResp)
 }
 
+func ConvertUSDtoEUR(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	api_key, _ := vars["API_key"]
+	usd, _ := vars["usd"]
+	usdFloat, _ := strconv.ParseFloat(usd,64)
+	fmt.Println(usdFloat)
+	if !isAuthorized(api_key) {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(NewResponse("Invalid API key", http.StatusUnauthorized))
+		return
+	}
+	resp, err := http.Get("http://data.fixer.io/api/2020-10-19?access_key=d2f10c93785cdb3dfd738ad60dca039d&symbols=USD&format=1")
+	if err != nil {
+		panic(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	var curr currency
+	err = json.Unmarshal(body, &curr)
+	response := make(map[string]float64)
+	response["Amount in EUR equals"]= usdFloat / curr.Rates.USD
+	jsonResp, err := json.Marshal(response)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Write(jsonResp)
+}
+
+func ConvertEURtoUSD(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	api_key, _ := vars["API_key"]
+	eur, _ := vars["eur"]
+	eurFloat, _ := strconv.ParseFloat(eur,64)
+	fmt.Println(eurFloat)
+	if !isAuthorized(api_key) {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(NewResponse("Invalid API key", http.StatusUnauthorized))
+		return
+	}
+	resp, err := http.Get("http://data.fixer.io/api/2020-10-19?access_key=d2f10c93785cdb3dfd738ad60dca039d&symbols=USD&format=1")
+	if err != nil {
+		panic(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	var curr currency
+	err = json.Unmarshal(body, &curr)
+	response := make(map[string]float64)
+	response["Amount in eur equals"]= eurFloat * curr.Rates.USD
+	jsonResp, err := json.Marshal(response)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Write(jsonResp)
+}
+
 
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	r := mux.NewRouter()
 	r.HandleFunc("/apikey", GetAPIkey).Methods("GET")
-	r.HandleFunc("/currency_rate", GetCurrency).Methods("GET").Queries("API_key" , "{API_key}")
+	r.HandleFunc("/currency_rate", GetCurrency).Methods("GET").Queries("api_key" , "{API_key}")
+	r.HandleFunc("/convert_usd", ConvertUSDtoEUR).Methods("GET").Queries("api_key" , "{API_key}" , "usd", "{usd}")
+	r.HandleFunc("/convert_eur", ConvertEURtoUSD).Methods("GET").Queries("api_key" , "{API_key}" , "eur", "{eur}")
 	fmt.Printf("Starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
