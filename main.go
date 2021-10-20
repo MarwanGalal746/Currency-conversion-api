238c88c0108506d70361e59fffa29d9f85fc6888
marwan <marwangalal746>
2021-10-20 12:06:30 +0200

    updating structure of the project

--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gorilla/mux"
-	"log"
+	"Currency_conversion_API/pkg/handlers"
 	"math/rand"
-	"net/http"
 	"time"
 )
 
 
-
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	r := mux.NewRouter()
-	r.HandleFunc("/apikey", GetAPIkey).Methods("GET")
-	r.HandleFunc("/currency_rate", GetCurrency).Methods("GET").Queries("api_key" , "{api_key}")
-	r.HandleFunc("/convert_usd", ConvertUSDtoEUR).Methods("GET").Queries("api_key" , "{api_key}" , "usd", "{usd}")
-	r.HandleFunc("/convert_eur", ConvertEURtoUSD).Methods("GET").Queries("api_key" , "{api_key}" , "eur", "{eur}")
-	fmt.Printf("Starting server at port 8080\n")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	handlers.Start()
 }
